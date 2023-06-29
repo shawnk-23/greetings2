b3276f3366feeae4221b2ac38facc46e0a0b20d2
shawnk-23 <hyunsung2018@engineer.com>
2023-06-28 22:14:46 -0700

    combined class and functional components

--- Greetings1.js
+++ Greetings1.js
@@ -9,14 +9,22 @@ class Greetings1 extends React.Component{
             
             return(
             <div>
-            <h1 className="text-center m-5 bg-primary text-white rounded-3">Greetings Page</h1>
             <h2 className="mx-5">Hello {names[randomNum]}!!</h2>
             </div>
             )
         }
     }
+    function Greetings2(){
+        // const names = props.names;
+        // const randomNum = Math.floor(Math.random() * names.length)
+        
+    
+        return (
+            <h1 className="text-center m-5 bg-primary text-white rounded-3">Greetings Page</h1>
+           
+        )
+    }
 
 
-
-const names = ['Sireesha','Nagireddy','Jane','Mike','David','Shawn','Bal','Brendan','Immaculate','Bruce','Yan','Walter','Benson','Audriana','Falguni']
-root.render(<Greetings1 names={names}/>)
+const names = ['Sireesha','Nagireddy','Jane','Mike','David','Shawn','Bal','Brendan','Immaculate','Bruce','Yan','Walter','Benson','Artin','Audriana','Falguni']
+root.render([<Greetings2/>,<Greetings1 names={names}/>])
